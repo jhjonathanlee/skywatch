9150671d4a8aa03792b60cdb90b54937f1c8cc8a
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-13 22:18:45 -0400

    code for retrieving weather

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
-function getWeather() {
+function getWeather(lat, lon) {
   console.log("getWeather");
-  var url = "https://fcc-weather-api.glitch.me/api/current?lat=42&lon=83";
+
+  var url = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + lat + "&lon=" + lon;
   $.getJSON(url, function(json) {
     $(".weather").text(json.weather[0].main + " " + json.weather[0].description);
   });
@@ -8,5 +9,7 @@ function getWeather() {
 
 $(document).ready(function() {
   console.log("ready");
-  getWeather();
+  navigator.geolocation.getCurrentPosition(function(position) {
+    getWeather(position.coords.latitude, position.coords.longitude);  
+  });
 });
