7549fb7ff4e0e1b79feb5217d13e526d4ff358a2
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-14 16:51:43 -0400

    shows temperature, icon, and weather text with basic styling

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 function getWeather(lat, lon) {
-  console.log("getWeather");
-
   var url = "https://fcc-weather-api.glitch.me/api/current?" + "lat=" + lat + "&lon=" + lon;
   $.getJSON(url, function(json) {
-    $(".weather").text(json.weather[0].main + " " + json.weather[0].description);
+    console.log(json);
+    var weather = json.weather[0];
+    $(".weather").text(weather.main);
+    $(".icon").attr("src", weather.icon);
+    $(".temp").html(json.main.temp + "&deg;C");
   });
 }
 
 $(document).ready(function() {
-  console.log("ready");
   navigator.geolocation.getCurrentPosition(function(position) {
     getWeather(position.coords.latitude, position.coords.longitude);  
   });
