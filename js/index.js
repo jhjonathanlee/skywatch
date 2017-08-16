fda655d3b9eccb0c3d5e80a74dfaf82e347189a1
Jonathan Lee <jh.jonathanlee@gmail.com>
2017-08-16 17:50:27 -0400

    temperature units selectable from dropdown

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var temp_f, temp_c;
+var isC;
+
 function getWeather(lat, lon) {
   var key = "1cf17322174e4235bdb35356171608";
   var url = "https://api.apixu.com/v1/current.json?key=" + key + "&q=" + lat + "," + lon;
@@ -7,14 +10,36 @@ function getWeather(lat, lon) {
     var current = json.current;
     $(".weather").text(current.condition.text);
     $(".icon").attr("src", current.condition.icon);
-    $(".temp").html(current.temp_c + "&deg;C");
+
+    temp_f = current.temp_f;
+    temp_c = current.temp_c;
+
+    var showTemp = isC ? temp_c : temp_f;
+
+    $(".temp").text(showTemp);
     var loc = json.location;
     $(".location").text(loc.name + ", " + loc.region + ", "  + loc.country);
   });
 }
 
 $(document).ready(function() {
-    navigator.geolocation.getCurrentPosition(function(position) {
+  isC = true;
+  temp_f = "-";
+  temp_c = "-";
+
+  navigator.geolocation.getCurrentPosition(function(position) {
     getWeather(position.coords.latitude, position.coords.longitude);  
+  });
+
+  $(".temp_c").click(function() {
+    isC = true;
+    $(".temp").text(temp_c);
+    $(".temp_units").html("C");
+  });
+
+  $(".temp_f").click(function() {
+    isC = false;
+    $(".temp").text(temp_f);
+    $(".temp-units").html("F");
   });
 });
